24852eca8ac5fc209fe0271c920be7ffd2519559
nekootoko3 <miyako.kerria@gmail.com>
2020-07-16 08:04:36 +0900

    Update index

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,67 @@
 import Head from "next/head";
 import styled from "styled-components";
-import { headingMd } from "../styles/font";
+import { headingMd, headingLg } from "../styles/font";
 import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <Heading>
+      <HeadingBio>
         <p>
           こんにちは、僕はいわゆる猫です。パソコンを使うことができるハイパーな猫です。
         </p>
         <p>最近の猫の毎日は平和です。</p>
-      </Heading>
+      </HeadingBio>
+      <HeadingBlog>
+        <HeadingBlogTitle>Blog</HeadingBlogTitle>
+        <BlogList>
+          {allPostsData.map(({ id, date, title }) => (
+            <BlogListItem>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </BlogListItem>
+          ))}
+        </BlogList>
+      </HeadingBlog>
     </Layout>
   );
 }
 
-const Heading = styled.div`
+const HeadingBio = styled.section`
   ${headingMd}
 `;
+
+const HeadingBlog = styled.section`
+  ${headingMd}
+  padding-top: 1px;
+`;
+
+const HeadingBlogTitle = styled.div`
+  ${headingLg}
+`;
+
+const BlogList = styled.ul`
+  list-style: none;
+  padding: 0px;
+  margin: 0px;
+`;
+
+const BlogListItem = styled.li`
+  margin: 0 0 1.25rem;
+`;
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
