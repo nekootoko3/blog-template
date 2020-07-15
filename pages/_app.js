c45ce860e956dbd50f5b3ce6ac1e77d9bb14b110
nekootoko3 <miyako.kerria@gmail.com>
2020-07-15 15:38:51 +0900

    Apply global style

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import App from "next/app";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 
 const theme = {
   colors: {
@@ -7,12 +7,43 @@ const theme = {
   },
 };
 
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+      Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    line-height: 1.6;
+    font-size: 18px;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  a {
+    color: #0070f3;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  img {
+    max-width: 100%;
+    display: block;
+  }
+`;
+
 export default class BlogTemplate extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
+        <GlobalStyle />
       </ThemeProvider>
     );
   }
