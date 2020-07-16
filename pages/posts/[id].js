835c36c34ca9f61dc0ac37921d3fa2d5b5780c8e
nekootoko3 <miyako.kerria@gmail.com>
2020-07-16 09:44:37 +0900

    Update posts pages

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,26 @@
+import Head from "next/head";
+
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1>{postData.title}</h1>
+        <div>
+          <Date dateString={postData.date} />
+        </div>
+        <br />
+        {postData.id}
+        <br />
+        {postData.date}
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
@@ -22,7 +34,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
